27944183a4858d87530c858356f71ff6c74f67d4
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-01-02 21:27:43 -0300

    .

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css"
+
+const home = () => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css"
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
