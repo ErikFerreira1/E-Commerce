be2baba12d09a2fbea1304267980d7bde2ee8ab9
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-01-08 23:37:59 -0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,26 +7,7 @@ const home = () => {
         <img src="#" />
         <button>shop now</button>
       </div>
-      <div>
-        <p>Frete</p>
-        <p>Cash</p>
-        <p>Garantia</p>
-      </div>
-      <div>
-        <p>Tudo</p>
-        <p>Relógios</p>
-        <p>Ternos</p>
-        <p>Calças</p>
-        <p>Tênis</p>
-      </div>
-      <div>
-        <img src="#" />
-        <img src="#" />
-        <img src="#" />
-        <img src="#" />
-        <img src="#" />
-        <img src="#" />
-      </div>
+
     </div>
   );
 };
