6f556bbe6a7728c5a9d5c9cf0d6005c4871f7e2a
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-01-15 18:50:34 -0300

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,17 +2,33 @@ import "./Home.css";
 
 const home = () => {
   return (
-    <div className="Home">
-      <div className="OsyrisImage">
-        <img src="../Images/Osyris.png" />
-      </div>
-      <div className="OsyrisDescription">
-        <h1>OSYRIS WATCH</h1>
-        <h2>O MAIS NOVO E MODERNO</h2>
-        <button type="button">
-          <a href="#">COMPRE AGORA</a>
-        </button>
-      </div>
+    <div>
+      <ul>
+        <li>
+          <div className="OsyrisImage">
+            <img src="../Images/Osyris.png" />
+            <div className="OsyrisDescription">
+              <h1>OSYRIS WATCH</h1>
+              <h2>O MAIS NOVO E MODERNO</h2>
+              <button type="button">
+                <a href="#">COMPRE AGORA</a>
+              </button>
+            </div>
+          </div>
+        </li>
+        <li>
+        <div className="VoguezImage">
+          <img src="../Images/Voguez.jpg"  />
+          <div className="VoguezDescription">
+              <h1>OSYRIS WATCH</h1>
+              <h2>O MAIS NOVO E MODERNO</h2>
+              <button type="button">
+                <a href="#">COMPRE AGORA</a>
+              </button>
+            </div>
+        </div>
+        </li>
+      </ul>
     </div>
   );
 };
